5e50f21bb0e14e5c2d548a41cc3ca65911923166
Omar <omared250@gmail.com>
2022-07-10 15:06:11 +0200

    working in a destination feature

---
+++ src/js/views/destination.js
@@ -0,0 +1,13 @@
+class Destination  {
+    _destinationEl = document.querySelector('#destination'); 
+    _parentEl = document.querySelector('.main_section');
+    _data;
+
+    _generateMarkup() {
+        return `
+        
+        `
+    }
+}
+
+export default new Destination();
