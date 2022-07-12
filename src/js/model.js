f6587d32b6ad209a2520aabceff02a995a42dbb5
Omar <omared250@gmail.com>
2022-07-12 22:46:22 +0200

    making the AJAX CALL API

--- src/js/model.js
+++ src/js/model.js
@@ -1,17 +1,28 @@
+const API = 'https://omared250.github.io/space_tourism_app/data.json';
+
+export const AJAX = async function(url) {
+    try {
+        const fetchData = fetch(url);
+        const res = await fetchData;
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const state = {};
 
 
-export const state = {
-    destination : [],
-};
-
-const destinationObject = function(dst) {
-    return {
-        name : dst.name,
-        image : dst.images.png,
-        info : dst.description,
-        distance : dst.distance,
-        travelDays : dst.travel,
+export const loadAll = async function() {
+    try {
+        const data = await AJAX(`${API}`);
+        state.destinations = Object.values(data.destinations);
+        state.crew = Object.values(data.crew);
+        state.technology = Object.values(data.technology);
+    } catch (err) {
+        throw err;
     }
 };
 
-console.log(data);
+loadAll();
