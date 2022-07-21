74bf8b397332a8ede57af00eb0c14e5bfccd9a67
Omar <omared250@gmail.com>
2022-07-21 15:05:25 +0200

    crew page responsive

--- src/js/views/crewView.js
+++ src/js/views/crewView.js
@@ -6,13 +6,48 @@ class crew {
     renderCrew(data) {
         this._data = data
         const markup = this._generateMarkupCrew();
-        this._bacgroundEl.style.backgroundImage = "url('./src/images/crew/background-crew-desktop.jpg')"
         this._parentEl.insertAdjacentHTML("beforeend", markup);
+        this._checkingWidthScreen();
+        this._backImage();
+    }
+
+    _checkingWidthScreen() {
+        if (screen.width > 1020) {
+            this._bacgroundEl.style.backgroundImage = "url('./src/images/crew/background-crew-desktop.jpg')";
+        } else if (screen.width < 1019) {
+            this._bacgroundEl.style.backgroundImage = "url('./src/images/crew/background-crew-tablet.jpg')";
+        } else if (screen.width < 769) {
+            this._bacgroundEl.style.backgroundImage = "url('./src/images/crew/background-crew-mobile.jpg')";
+        }
+    }
+
+    _backImage() {
+        const backDesktopImage = window.matchMedia("(min-width: 800px)");
+        const backTabletpImage = window.matchMedia("(max-width: 769px)");
+        const backMobilepImage = window.matchMedia("(max-width: 430px)");
+        backDesktopImage.onchange = (e) => {
+            if (e.matches) {
+                this._bacgroundEl.style.backgroundImage = "url('./src/images/crew/background-crew-desktop.jpg')";
+            }
+        }
+
+        backTabletpImage.onchange = (e) => {
+            if (e.matches) {
+                this._bacgroundEl.style.backgroundImage = "url('./src/images/crew/background-crew-tablet.jpg')";
+            }
+        }
+
+        backMobilepImage.onchange = (e) => {
+            if (e.matches) {
+                this._bacgroundEl.style.backgroundImage = "url('./src/images/crew/background-crew-mobile.jpg')";
+            }
+        }
+        
     }
 
     _generateMarkupCrew() {
         return `
-        <h3 class="destination__title"><span>02</span>MEET YOUR CREW</h3>
+        <h3 class="crew__title"><span>02</span>MEET YOUR CREW</h3>
         <div class="slider">
             <div class="slide slide--1">
                 <div class="biografia">
