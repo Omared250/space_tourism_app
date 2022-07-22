fdff2df50799f000dd1b5a443e1664901822aba8
Omar <omared250@gmail.com>
2022-07-22 14:50:09 +0200

    tech view responsive and refcatoring css

--- src/js/views/techView.js
+++ src/js/views/techView.js
@@ -6,9 +6,22 @@ class technology {
     renderTech(data) {
         this._data = data
         const markup = this._generateMarkupTech();
-        this._bacgroundEl.style.backgroundImage = "url('./src/images/technology/background-technology-desktop.jpg')"
         this._parentEl.insertAdjacentHTML("beforeend", markup);
+        this._checkingWidthScreen();
+        this._backImage();
+    }
 
+    _checkingWidthScreen() {
+        if (screen.width > 1020) {
+            this._bacgroundEl.style.backgroundImage = "url('./src/images/technology/background-technology-desktop.jpg')";
+        } else if (screen.width < 1019) {
+            this._bacgroundEl.style.backgroundImage = "url('./src/images/technology/background-technology-tablet.jpg')";
+        } else if (screen.width < 769) {
+            this._bacgroundEl.style.backgroundImage = "url('./src/images/technology/background-technology-mobile.jpg')";
+        }
+    }
+
+    _backImage() {
         const backDesktopImage = window.matchMedia("(min-width: 800px)");
         const backTabletpImage = window.matchMedia("(max-width: 769px)");
         const backMobilepImage = window.matchMedia("(max-width: 430px)");
@@ -31,9 +44,20 @@ class technology {
         }
     }
 
+    _image(descData) {
+        const descImage = window.matchMedia("(max-width: 769px)");
+        return descImage.onchange = (e) => {
+            if (e.matches) {
+                return descData.images.landscape;
+            } else {
+                return descData.images.portrait;
+            }
+        }
+    }
+
     _generateMarkupTech() {
         return `
-        <h3 class="destination__title"><span>03</span>SPACE LAUNCH 101</h3>
+        <h3 class="tech__title"><span>03</span>SPACE LAUNCH 101</h3>
         <section class="tech__container">
             <article class="description__container">
                 <div class="tech_dots">
@@ -43,11 +67,11 @@ class technology {
                 </div>
                 <div class="description">
                     <h4>THE TERMINOLOGY...</h4>
-                    <h2>${this._data[0].name}</h2>
+                    <h2>${this._data[0].name.toUpperCase()}</h2>
                     <blockquote>${this._data[0].description}</blockquote>
                 </div>
             </article>
-            <img src="${this._data[0].images.portrait}" alt="tech__image">
+            <img src="${this._data[0].images.portrait}" alt="tech__image class="tech__image">
         </section>`
     }
 
